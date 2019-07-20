351c9d939e22077a750a939087d2c1493f032f06
Dolcevice <Dolcevice@users.noreply.github.com>
2019-07-20 11:58:56 -0600

    added a directory of py solutions

---
+++ Py Solutions/Rosalind 6 - DNA Splicing.py
@@ -0,0 +1,9 @@
+# Started : 2019/07/20
+# Dolcevice/Yun Ho Jung
+
+# Given: A DNA string s (of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are
+# given in FASTA format.
+
+# Return: A protein string resulting from transcribing and translating the exons of s. (Note: Only one solution will
+# exist for the dataset provided.)
+
