d7f3eeee1cdeef083cbfaec084f529a377420a20
Dolcevice <Dolcevice@users.noreply.github.com>
2019-07-21 20:56:02 -0600

    Improved time complexity
    O(n^2) -> O(n)

--- Rosalind 7 - Locating Restriction Sites/main.py
+++ Rosalind 7 - Locating Restriction Sites/main.py
@@ -1,7 +1,7 @@
 comp_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 target = input()
 target = target.replace(' ', '')
-
+print()
 
 # returns the reverse compliment of DNA strand
 def get_recomp(target_str):
@@ -17,13 +17,17 @@ loc_list = []
 len_list = []
 
 # Search through the string to find rp
-# n^2 time complexity
+# O(n) time complexity
 for index1 in range(len(target)):
-    for index2 in range(len(target) - index1 - 3):
+    if index1 + 4 > len(target):
+        break
+    for index2 in range(9):
+        if index1 + index2 + 4 > len(target):
+            break
         sub = target[index1: index1 + index2 + 4]
         if sub == get_recomp(sub):
             loc_list.append(index1 + 1)
-            len_list.append(index2 + 4)
+            len_list.append(len(sub))
 
 # print the result
 for (loc, leng) in zip(loc_list, len_list):
